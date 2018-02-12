b953c63d781247011b97d185780b7be2a952faa7
Tyler Benson <tyler@newrelic.com>
2018-02-12 14:00:43 +1000

    Add resource naming instrumentation for jax-rs

--- dd-java-agent/tooling/src/main/java/datadog/trace/agent/tooling/AgentInstaller.java
+++ dd-java-agent/tooling/src/main/java/datadog/trace/agent/tooling/AgentInstaller.java
@@ -1,13 +1,5 @@
 package datadog.trace.agent.tooling;
 
-import static datadog.trace.agent.tooling.ClassLoaderMatcher.classLoaderWithName;
-import static datadog.trace.agent.tooling.ClassLoaderMatcher.isReflectionClassLoader;
-import static net.bytebuddy.matcher.ElementMatchers.any;
-import static net.bytebuddy.matcher.ElementMatchers.isBootstrapClassLoader;
-import static net.bytebuddy.matcher.ElementMatchers.nameContains;
-import static net.bytebuddy.matcher.ElementMatchers.nameMatches;
-import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
-
 import java.lang.instrument.Instrumentation;
 import java.util.ServiceLoader;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +8,14 @@ import net.bytebuddy.agent.builder.ResettableClassFileTransformer;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.utility.JavaModule;
+
+import static datadog.trace.agent.tooling.ClassLoaderMatcher.classLoaderWithName;
+import static datadog.trace.agent.tooling.ClassLoaderMatcher.isReflectionClassLoader;
+import static net.bytebuddy.matcher.ElementMatchers.any;
+import static net.bytebuddy.matcher.ElementMatchers.isBootstrapClassLoader;
+import static net.bytebuddy.matcher.ElementMatchers.nameContains;
+import static net.bytebuddy.matcher.ElementMatchers.nameMatches;
+import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
 
 @Slf4j
 public class AgentInstaller {
@@ -26,33 +26,36 @@ public class AgentInstaller {
    * @param inst Java Instrumentation used to install bytebuddy
    * @return the agent's class transformer
    */
-  public static ResettableClassFileTransformer installBytebuddyAgent(final Instrumentation inst) {
+  public static ResettableClassFileTransformer installBytebuddyAgent(final Instrumentation inst, final AgentBuilder.Listener... listeners) {
     AgentBuilder agentBuilder =
-        new AgentBuilder.Default()
-            .disableClassFormatChanges()
-            .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
-            .with(new LoggingListener())
-            .ignore(nameStartsWith("datadog.trace."))
-            .or(nameStartsWith("datadog.opentracing."))
-            .or(nameStartsWith("datadog.slf4j."))
-            .or(nameStartsWith("java."))
-            .or(nameStartsWith("com.sun."))
-            .or(nameStartsWith("sun."))
-            .or(nameStartsWith("jdk."))
-            .or(nameStartsWith("org.aspectj."))
-            .or(nameStartsWith("org.groovy."))
-            .or(nameStartsWith("com.p6spy."))
-            .or(nameStartsWith("org.slf4j."))
-            .or(nameContains("javassist"))
-            .or(nameContains(".asm."))
-            .or(nameMatches("com\\.mchange\\.v2\\.c3p0\\..*Proxy"))
-            .ignore(
-                any(),
-                isBootstrapClassLoader()
-                    .or(isReflectionClassLoader())
-                    .or(
-                        classLoaderWithName(
-                            "org.codehaus.groovy.runtime.callsite.CallSiteClassLoader")));
+      new AgentBuilder.Default()
+        .disableClassFormatChanges()
+        .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+        .with(new LoggingListener())
+        .ignore(nameStartsWith("datadog.trace."))
+        .or(nameStartsWith("datadog.opentracing."))
+        .or(nameStartsWith("datadog.slf4j."))
+        .or(nameStartsWith("java."))
+        .or(nameStartsWith("com.sun."))
+        .or(nameStartsWith("sun."))
+        .or(nameStartsWith("jdk."))
+        .or(nameStartsWith("org.aspectj."))
+        .or(nameStartsWith("org.groovy."))
+        .or(nameStartsWith("com.p6spy."))
+        .or(nameStartsWith("org.slf4j."))
+        .or(nameContains("javassist"))
+        .or(nameContains(".asm."))
+        .or(nameMatches("com\\.mchange\\.v2\\.c3p0\\..*Proxy"))
+        .ignore(
+          any(),
+          isBootstrapClassLoader()
+            .or(isReflectionClassLoader())
+            .or(
+              classLoaderWithName(
+                "org.codehaus.groovy.runtime.callsite.CallSiteClassLoader")));
+    for (final AgentBuilder.Listener listener : listeners) {
+      agentBuilder = agentBuilder.with(listener);
+    }
     int numInstrumenters = 0;
     for (final Instrumenter instrumenter : ServiceLoader.load(Instrumenter.class)) {
       log.debug("Loading instrumentation {}", instrumenter);
@@ -74,7 +77,8 @@ public class AgentInstaller {
         final JavaModule module,
         final boolean loaded,
         final Throwable throwable) {
-      log.debug("Failed to handle " + typeName + " for transformation: " + throwable.getMessage());
+      log.warn("Failed to handle " + typeName + " for transformation: " + throwable.getMessage());
+      log.warn("ERROR: ", throwable);
     }
 
     @Override
